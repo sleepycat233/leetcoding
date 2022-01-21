d831ce66269d73afd9a53efa059462289fb29a5d
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-01-21 11:47:33 +0900

    Time: 2 ms (77.89%), Space: 37.3 MB (78.99%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.java
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -1,40 +1,21 @@
 class Solution {
     public int longestPalindrome(String str) {
-                int[] t = new int[128];
+                int len = 0;
+        int[] t = new int[128];
+
         for(int i = 0; i < str.length(); i++) {
-            t[str.charAt(i)]++;
+            char c;
+            t[c = str.charAt(i)]++;
+            if(t[c] % 2 == 0) len+=2;
         }
 
-        // System.out.println(Arrays.toString(t));
-        // int sum = 0, sumofeven = 0;
-        // for(int i = 0; i < t.length; i++) {
-        //     sum += t[i];
-        //     if(t[i] != 0 && t[i]%2==1) {
-        //         System.out.println((char)i + " appears " + t[i] + " times.");
-        //     } else {
-        //         sumofeven += t[i];
-        //     }
-        // }
-        // System.out.println("sum: " + sum + " sumofeven: " + sumofeven);
-
-        int len = 0;
-        int maxmid = 0;
         for(int x : t) {
-            if(x % 2 == 0) {
-                len += x;
-            }
-            else { //if(x % 2 == 1)
-                if (x > maxmid) {
-                    len = len + x - maxmid;
-                    len += maxmid / 2 * 2;
-                    maxmid = x;
-                }
-                else {
-                    len += x / 2 * 2;
-                }
+            if(x % 2 == 1) {
+                len+=1;
+                break;
             }
         }
-        return len;
 
+        return len;
     }
 }
