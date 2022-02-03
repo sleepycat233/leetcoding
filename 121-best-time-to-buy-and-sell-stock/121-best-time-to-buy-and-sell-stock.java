714fdffd87ad1ecb549f90598fd106ca010223a9
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-02-03 14:42:11 +0900

    Time: 4 ms (30.94%), Space: 83.3 MB (56.71%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -6,9 +6,6 @@ class Solution {
         for(int i = 1; i < prices.length; i++) {
             if(prices[i] > prices[selli]) {
                 selli = i;
-                if(prices[i-1] < prices[buyi]) {
-                    buyi = i-1;
-                }
                 maxp = Math.max(maxp, prices[selli] - prices[buyi]);
             }
             else if (prices[i] < prices[buyi]) {
