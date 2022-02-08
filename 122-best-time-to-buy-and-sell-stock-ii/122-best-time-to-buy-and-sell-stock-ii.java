922045fa7aa9822b84059d4dc53c78daa4bcc59a
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-02-08 10:22:26 +0900

    Time: 2 ms (31.62%), Space: 41.8 MB (22.62%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,20 +1,22 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int total = 0;
-        int unrealizedProfit = 0;
+            int[][] dp = new int[prices.length][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
 
         for(int i = 1; i < prices.length; i++) {
-            if(prices[i] < prices[i-1]) {
-                if(unrealizedProfit != 0) {
-                    total += unrealizedProfit;
-                    unrealizedProfit = 0;
-                }
-            }
-            else {
-                unrealizedProfit += prices[i] - prices[i-1];
-            }
-        }
-        total += unrealizedProfit;
+            //两种选择，空仓还是持仓
 
-        return total;    }
+            //空仓：两种操作
+            //1。继续空仓-延续昨日空仓收益
+            //2。卖出昨日持有的股票实现空仓
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]+prices[i]);
+            //持仓：两种操作
+            //1. 持仓昨日的股票
+            //2. 买入今日的股票
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0]-prices[i]);
+        }
+
+        return dp[prices.length-1][0];
+    }
 }
