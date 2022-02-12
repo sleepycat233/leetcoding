bbfed7787de4ca455b0cbe430f4121d4ccd90c51
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-02-12 15:36:07 +0900

    Time: 136 ms (10.08%), Space: 76.2 MB (11.95%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,23 +1,21 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        int[] a = new int[n];
-        int dead = 0;
-        int index = 0;
-        int count = 1;
-        while(dead < n) {
-            while(count < k) {
-                if (a[index % n] != 1) {
-                    index++;
-                    count++;
-                }
-                else {
-                    index++;
-                }
-            }
-            count = 1;
-            while(a[index % n] == 1) index++;
-            a[index%n] = 1;
-            dead++;
+        Deque<Integer> d = new LinkedList<>();
+        for(int i = 1; i <= n; i++) {
+            d.add(i);
         }
-        return (index % n) + 1;    }
+
+        int count = 1;
+        while(d.size() != 1) {
+            if(count == k) {
+                d.removeFirst();
+                count = 1;
+            }
+            else {
+                d.addLast(d.removeFirst());
+                count++;
+            }
+        }
+        return d.getFirst();
+    }
 }
