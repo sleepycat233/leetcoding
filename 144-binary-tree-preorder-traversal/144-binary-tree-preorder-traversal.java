6f03789168a2588579d14765a8a15936a49e1cba
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-02-15 16:10:12 +0900

    Time: 1 ms (38.70%), Space: 42.3 MB (13.48%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -14,18 +14,21 @@
  * }
  */
 class Solution {
-    
-        public List<Integer> preorderTraversal(TreeNode root)
-    {
+    public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
-        preorder(root, res);
-        return res;
-    }
+        Deque<TreeNode> s = new LinkedList<>();
+        if (root == null) return res;
 
-    private void preorder(TreeNode node, List<Integer> res) {
-        if(node == null) return;
-        res.add(node.val);
-        preorder(node.left, res);
-        preorder(node.right, res);
+        while(!s.isEmpty() || root != null) {
+            while(root != null) {
+                res.add(root.val);
+                s.push(root);
+                root = root.left;
+            }
+            root = s.pop();
+            root = root.right;
+        }
+
+        return res;
     }
 }
