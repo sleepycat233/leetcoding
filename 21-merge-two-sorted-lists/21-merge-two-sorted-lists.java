d227113a86db07ab90a8713ea721ebea694dd54c
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-02-21 13:57:02 +0900

    Time: 1 ms (65.26%), Space: 43.8 MB (6.59%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -10,29 +10,27 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummy = new ListNode(-1);
-        ListNode res = dummy;
-        while(list1 != null || list2 != null) {
-            if(list1 == null) {
-                res.next = list2;
-                break;
-            }
-            else if(list2 == null) {
-                res.next = list1;
-                break;
-            }
-            else if (list1.val <= list2.val) {
-                res.next = new ListNode(list1.val);
-                res = res.next;
-                list1 = list1.next;
-            }
-            else {
-                res.next = new ListNode(list2.val);
-                res = res.next;
-                list2 = list2.next;
-            }
+        ListNode res = null;
+        if (list1 != null || list2 != null) {
+            res = merge_rec(list1, list2);
         }
+        return res;
+    }
 
-        return dummy.next;
+    private ListNode merge_rec(ListNode l1, ListNode l2) {
+        if(l1 == null) {
+            // return new ListNode(l2.val);
+            return l2;
+        }
+        else if(l2 == null) {
+            // return new ListNode(l1.val);
+            return l1;
+        }
+        else if(l1.val < l2.val) {
+            return new ListNode(l1.val, merge_rec(l1.next, l2));
+        }
+        else {
+            return new ListNode(l2.val, merge_rec(l2.next, l1));
+        }
     }
 }
