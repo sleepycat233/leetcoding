bd7cac1d769b91af130d3b7d4ac273c637398d61
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-03-08 17:28:58 +0900

    Time: 0 ms (100.00%), Space: 39.4 MB (63.22%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.java
+++ 226-invert-binary-tree/226-invert-binary-tree.java
@@ -15,16 +15,22 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        if(root == null) {
-            return null;
-        }
-        else {
-            TreeNode l = invertTree(root.left);
-            TreeNode r = invertTree(root.right);
-            root.left = r;
-            root.right = l;
-            return root;
-        }
-    }
+        List<TreeNode> prev = new ArrayList<>();
+        List<TreeNode> curr = new ArrayList<>();
+        if(root != null) prev.add(root);
 
+        while(!prev.isEmpty()) {
+            curr = new ArrayList<>();
+            for(TreeNode n : prev) {
+                TreeNode t = (n != null) ? n.left : null;
+                n.left = n.right;
+                n.right = t;
+                if(n.left != null) curr.add(n.left);
+                if(n.right != null) curr.add(n.right);
+            }
+            prev = curr;
+        }
+
+        return root;
+    }
 }
