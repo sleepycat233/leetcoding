d535c3fdd92bcde7c9bfc3343902851fef8e31d7
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-03-15 09:13:25 +0900

    Time: 2 ms (14.62%), Space: 43.6 MB (5.12%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -15,21 +15,23 @@
  */
 class Solution {
     public void flatten(TreeNode root) {
-        List<TreeNode> nodes = new ArrayList<>();
         Deque<TreeNode> s = new ArrayDeque<>();
+        TreeNode prev = null;
 
-        if(root != null) s.push(root);
-        while(!s.isEmpty()) {
-            TreeNode n = s.pop();
-            while(n!=null) {
-                if(n.right != null) s.push(n.right);
-                nodes.add(n);
-                n = n.left;
+        if (root != null) s.push(root);
+
+        while (!s.isEmpty()) {
+            TreeNode curr = s.pop();
+            while (curr != null) {
+                TreeNode next = curr.left;
+                if (curr.right != null) s.push(curr.right);
+                if (prev != null) {
+                    prev.left = null;
+                    prev.right = curr;
+                }
+                prev = curr;
+                curr = next;
             }
-        }
-        for (int i = 0; i < nodes.size() - 1; i++) {
-            nodes.get(i).left = null;
-            nodes.get(i).right = nodes.get(i + 1);
         }
     }
 }
