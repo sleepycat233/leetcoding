5a6c26ea0f41d32e71bdb7d93a2b860b669e960e
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-03-19 22:11:35 +0900

    mergesort

--- leetcode_src/src/LinkedListUtil.java
+++ leetcode_src/src/LinkedListUtil.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 
 class ListNode {
     int val;
---
+++ practice/algo/sorting/MergeSort.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class MergeSort {
+    static public int[] sort(int[] a) {
+        return divide(a, 0, a.length-1);
+    }
+
+    static private int[] divide(int[] a, int l, int r) {
+        if(l == r) {
+            return new int[]{a[l]};
+        }
+        else {
+            int mid = (l+r)/2;
+            return merge(divide(a, l, mid), divide(a, mid+1, r));
+        }
+    }
+
+    static private int[] merge(int[] l1, int[] l2) {
+        int i = 0, j = 0, k = 0;
+        int[] res = new int[l1.length+l2.length];
+
+        while(i < l1.length && j < l2.length) {
+            if(l1[i] < l2[j]) {
+                res[k++] = l1[i++];
+            }
+            else {
+                res[k++] = l2[j++];
+            }
+        }
+        while(i < l1.length) {
+            res[k++] = l1[i++];
+        }
+        while(j < l2.length) {
+            res[k++] = l2[j++];
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[]{3, 1, 2, 5};
+        int[] res = MergeSort.sort(a);
+        System.out.println(Arrays.toString(res));
+    }
+
+}
