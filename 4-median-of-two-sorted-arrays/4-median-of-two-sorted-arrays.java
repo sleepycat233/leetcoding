f7351ca87a6c88cc426555422b68ac013d03ac5f
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-03-22 11:20:08 +0900

    Time: 5 ms (41.72%), Space: 50.3 MB (18.90%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -1,34 +1,66 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2)
-    {
-        int[] a = mergeTwoSortedArray(nums1, nums2);
-        double midian = (double)(a[a.length/2] + a[(int)((double)a.length/2 - 0.5)]) / 2;
-        return midian;
-    }
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 
-    private int[] mergeTwoSortedArray(int[] nums1, int[] nums2)
-    {
-        int m = nums1.length;
-        int n = nums2.length;
-        int[] res = new int[m + n];
-        int i = 0, j = 0, k = 0;
+        int m1 = 0, m2 = 0;
+        boolean rdone = false, ldone = false, found = false;
+        int l1 = 0, r1 = (nums1 != null) ? nums1.length-1 : -1;
+        int l2 = 0, r2 = (nums2 != null) ? nums2.length-1 : -1;
+        // if(nums1 == null) ldone = true;
+        // if(nums2 == null) rdone = true;
 
-        while (j < m && k < n) {
-            if (nums1[j] <= nums2[k]) {
-                res[i++] = nums1[j++];
+        while(found == false) {
+
+            // System.out.format("%d, %d, %d, %d\n", l1, r1, l2, r2);
+            if(l1 > r1) {
+                ldone = true;
+            }
+            else if(l2 > r2) {
+                rdone = true;
+            }
+
+            if(l1 == r1 && l2 == r2) {
+                found = true;
+                m1 = nums1[l1];
+                m2 = nums2[l2];
+            }
+            else if (rdone == true && (r1 - l1 == 1 || l1 == r1)) {
+                found = true;
+                m1 = nums1[l1];
+                m2 = nums1[r1];
+            }
+            else if (ldone == true && (r2 - l2 == 1 || l2 == r2)) {
+                found = true;
+                m1 = nums2[l2];
+                m2 = nums2[r2];
             }
             else {
-                res[i++] = nums2[k++];
+            }
+
+            if(!rdone && !ldone) {
+                if(nums1[l1] < nums2[l2]) {
+                    l1++;
+                }
+                else {
+                    l2++;
+                }
+                if(nums1[r1] > nums2[r2]) {
+                    r1--;
+                }
+                else {
+                    r2--;
+                }
+            }
+            else if(rdone) {
+                l1++;
+                r1--;
+            }
+            else {
+                l2++;
+                r2--;
             }
         }
 
-        while (j < m) {
-            res[i++] = nums1[j++];
-        }
-        while (k < n) {
-            res[i++] = nums2[k++];
-        }
-
-        return res;
+        return (double)(m1+m2) / 2;
+    
     }
 }
