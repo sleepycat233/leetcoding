4cc5061be10f8f063cbebd16727cf3fce9db827d
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-03-25 13:17:53 +0900

    Time: 4 ms (79.42%), Space: 44.1 MB (40.35%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -2,10 +2,8 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
         int[] count = new int[26];
-        for(int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
-        }
+        for(char c : s.toCharArray()) count[c-'a']++;
+        for(char c : t.toCharArray()) count[c-'a']--;
         for(int j = 0; j < 26; j++) {
             if(count[j] != 0) return false;
         }
