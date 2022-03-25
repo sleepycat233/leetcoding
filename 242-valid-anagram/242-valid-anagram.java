8fa03beebccc95f4440b376b823c657166443892
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-03-25 12:14:01 +0900

    Time: 1011 ms (5.19%), Space: 43.7 MB (56.11%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Deque<Character> stack = new ArrayDeque<>();
+        if(s.length() != t.length()) return false;
+
+        for(int i = 0, j = 0; i < s.length(); i++) {
+            stack.push(s.charAt(i));
+            while(!stack.isEmpty() && stack.contains(t.charAt(j))) {
+                stack.remove(t.charAt(j));
+                j++;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
