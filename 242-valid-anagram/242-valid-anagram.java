5a060d21c7e1e7c9c182f7e0c1557043b7f9f135
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-03-25 12:59:01 +0900

    Time: 81 ms (5.19%), Space: 49.9 MB (5.03%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,17 +1,11 @@
 class Solution {
+
     public boolean isAnagram(String s, String t) {
-
-        Deque<Character> stack = new LinkedList<>();
-        if(s.length() != t.length()) return false;
-
-        for(int i = 0, j = 0; i < s.length(); i++) {
-            stack.push(s.charAt(i));
-            while(!stack.isEmpty() && stack.contains(t.charAt(j))) {
-                stack.remove(t.charAt(j));
-                j++;
-            }
-        }
-        return stack.isEmpty();
-    
+        s = sortstr(s);
+        t = sortstr(t);
+        return s.equals(t);
+    }
+    private String sortstr(String str) {
+        return Stream.of(str.split("")).sorted().collect(Collectors.joining());
     }
 }
