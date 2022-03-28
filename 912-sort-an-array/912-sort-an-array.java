bf242f4f87a82c6d8af48150bc832cb3bbcaffe1
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-03-28 23:20:25 +0900

    Time: 10 ms (89.42%), Space: 67.2 MB (64.61%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,38 +1,6 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        quicksort(nums, 0, nums.length-1);
+        Arrays.sort(nums);
         return nums;
-    }
-
-    private void quicksort(int[] nums, int l, int r) {
-        if(l >= r) {
-            return;
-        }
-        else {
-            int pivot = partition(nums, l, r);
-            quicksort(nums, l, pivot-1);
-            quicksort(nums, pivot+1, r);
-        }
-    }
-
-    private int partition(int[] nums, int l, int r) {
-        int pivotv = nums[r];
-
-        int i = l-1, j = l;
-        while(j < r) {
-            if(nums[j] < pivotv) {
-                i++;
-                swap(nums, i, j);
-            }
-            j++;
-        }
-        swap(nums, i+1, r);
-        return i+1;
-    }
-
-    private void swap(int[] nums, int l, int r) {
-        int t = nums[l];
-        nums[l] = nums[r];
-        nums[r] = t;
     }
 }
