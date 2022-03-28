e256ce0d40caf0985d39e8de99f428f97f90418c
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-03-28 23:19:27 +0900

    Time: 1785 ms (8.13%), Space: 56.5 MB (78.35%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,52 +1,38 @@
 class Solution {
-    public int[] sortArray(int[] nums)
-    {
-        mergesort(nums);
+    public int[] sortArray(int[] nums) {
+        quicksort(nums, 0, nums.length-1);
         return nums;
     }
 
-    private void mergesort(int[] nums) {
-        int len = 1;
-        int n = nums.length;
-        int i;
-
-        while(len < n) {
-            i = 0;
-            while(i < n) {
-                int l1 = i;
-                int r1 = i+len-1;
-                int l2 = i+len;
-                int r2 = i+len*2-1;
-                if(r1 >= n) break;
-                if(r2 >= n) r2 = n-1;
-                merge(nums, l1, r1, l2, r2);
-
-                i = i+len*2;
-            }
-            len = len * 2;
+    private void quicksort(int[] nums, int l, int r) {
+        if(l >= r) {
+            return;
+        }
+        else {
+            int pivot = partition(nums, l, r);
+            quicksort(nums, l, pivot-1);
+            quicksort(nums, pivot+1, r);
         }
     }
 
-    private void merge(int[] nums, int l1, int r1, int l2, int r2) {
-        int[] tmp = new int[r2-l1+1];
-        int i = l1, j = l2, k = 0;
-        while(i <= r1 && j <= r2) {
-            if(nums[i] <= nums[j]) {
-                tmp[k++] = nums[i++];
-            }
-            else {
-                tmp[k++] = nums[j++];
-            }
-        }
-        while (i <= r1) {
-            tmp[k++] = nums[i++];
-        }
-        while (j <= r2) {
-            tmp[k++] = nums[j++];
-        }
+    private int partition(int[] nums, int l, int r) {
+        int pivotv = nums[r];
 
-        for(int m = 0; m < tmp.length; m++) {
-            nums[l1 + m] = tmp[m];
+        int i = l-1, j = l;
+        while(j < r) {
+            if(nums[j] < pivotv) {
+                i++;
+                swap(nums, i, j);
+            }
+            j++;
         }
+        swap(nums, i+1, r);
+        return i+1;
+    }
+
+    private void swap(int[] nums, int l, int r) {
+        int t = nums[l];
+        nums[l] = nums[r];
+        nums[r] = t;
     }
 }
