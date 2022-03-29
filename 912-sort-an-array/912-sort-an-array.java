4635ba3ad281302d66a991bcf1d840baf94166e8
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-03-29 11:27:04 +0900

    Time: 1438 ms (11.04%), Space: 50.6 MB (99.14%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,6 +1,15 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
+        for(int j = 1; j < nums.length; j++) {
+            int v = nums[j];
+            int i = j-1;
+            while (i >= 0 && nums[i] > v) {
+                nums[i+1] = nums[i];
+                i--;
+            }
+            nums[i+1] = v;
+        }
+
         return nums;
     }
 }
