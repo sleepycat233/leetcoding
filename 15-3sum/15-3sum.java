9b5b9f5c1402ff5eb0b9c071c8fafb7c7936a24f
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-04-12 18:33:09 +0900

    Time: 29 ms (65.58%), Space: 61.2 MB (28.17%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,37 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] n) {
+
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(n);
-        System.out.println(Arrays.toString(n));
+        // System.out.println(Arrays.toString(n));
 
-        for(int i = 0; i < n.length-2; i++) {
-            if(i == 0 || n[i-1] != n[i]) {
-                // for (int k = n.length - 1; k > i+1; k--) {
-                int k = n.length - 1;
-                int j = i+1;
+        for(int l = 0; l < n.length-2; l++) {
+            if(l != 0 && n[l-1] == n[l]) {
+                continue;
+            }
+            int r = n.length - 1;
+            for(int m = l+1; m < r; m++) {
+                if(m != l+1 && n[m-1] == n[m]) {
+                    continue;
+                }
 
-                while(j < k) {
-                    while(k != n.length-1 && n[k+1] == n[k] && k > j) {
-                        k--;
-                    }
-                    if(j >= k) break;
+                int target = -(n[l] + n[m]);
+                while(r > m && n[r] > target) r--;
+                if(m == r) break;
 
-                    int theone = -(n[i] + n[k]);
-                    if(n[j] == theone) {
-                        res.add(new ArrayList<>(Arrays.asList(n[i], n[j], n[k])));
-                        j++;
-                        k--;
-                    }
-                    else if(n[j] < theone) {
-                        j++;
-                    }
-                    else {
-                        k--;
-                    }
+                if(n[r] == target) {
+                    res.add(new ArrayList<>(Arrays.asList(n[l], n[m], n[r])));
                 }
             }
         }
 
         return res;
+    
     }
 }
