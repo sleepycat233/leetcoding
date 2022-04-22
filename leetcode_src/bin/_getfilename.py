c3a8f7c7deecd219a0f00e4456d49337b3df27a2
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-04-22 14:50:50 +0900

    getFileName.py - use StringIO

--- leetcode_src/bin/_getfilename.py
+++ leetcode_src/bin/_getfilename.py
@@ -1,8 +1,21 @@
-def getFileName():
-    title = "Construct Binary Tree from Preorder and Inorder Traversal"
-    res = ""
-    for s in title.split():
-        res += s.capitalize()
-    print(res)
+import sys
+import io
 
-getFileName()
+def getFileName(title):
+    # title = "Construct Binary Tree from Preorder and Inorder Traversal"
+    # res = ""
+    res = io.StringIO()
+    for s in title:
+        if not s.isupper():
+            # res += s.capitalize()
+            res.write(s.capitalize())
+        else:
+            # res += s
+            res.write(s)
+
+    res.write(".java")
+    print(res.getvalue())
+    res.close()
+
+
+getFileName(sys.argv[1:])
