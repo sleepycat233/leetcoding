f2a14276f23b47243dc18f41a5aa68399494331e
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-04-22 15:36:49 +0900

    Time: 0 ms (100.00%), Space: 41 MB (33.91%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -1,6 +1,13 @@
 class Solution {
-    int[] cheat = new int[]{1, 2, 5, 14, 42, 132, 429, 1430, 4862, 16796, 58786, 208012, 742900, 2674440, 9694845, 35357670, 129644790, 477638700, 1767263190};
     public int numTrees(int n) {
-        return cheat[n-1];
+        if(n == 1) return 1;
+
+        double ans = 1;
+        for(int k = 2; k <= n; k++) {
+            ans *= (double)(n+k) / k;
+        }
+
+        // return (int) ans; //gives wrong answer
+        return (int)Math.round(ans);
     }
 }
