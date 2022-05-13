ac35306eb166c5909cc2ffee30824df16ce19e3b
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-05-13 18:39:59 +0900

    Time: 86 ms (17.43%), Space: 38.9 MB (94.84%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -1,20 +1,16 @@
 class Solution {
-    private static final int VOWEL_COUNT = 5;
-    private int count = 0;
     public int countVowelStrings(int n) {
-        generateStirngs2(n, 0, 0);
-        return count;
+        return countv(n, 1);
     }
 
-    private void generateStirngs2(int n, int digits, int start) {
-        if(digits == n) {
-            ++count;
-            return;
-        }
+    private int countv(int n, int pos) {
+        if(n == 0) return 1;
         else {
-            for(int i = start; i < VOWEL_COUNT; i++) {
-                generateStirngs2(n, digits+1, i);
+            int result = 0;
+            for(int v = pos; v <= 5; v++) {
+                result += countv(n - 1, v);
             }
+            return result;
         }
     }
 }
