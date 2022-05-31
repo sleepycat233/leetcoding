965e7b312f629698f863d02d464c4a9077742067
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-05-31 19:37:53 +0900

    Time: 5 ms (58.61%), Space: 44.6 MB (27.38%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,36 +1,35 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 
-        Map<Integer, Integer> map = new LinkedHashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         Deque<Integer> stack = new ArrayDeque<>();
+        int[] res = new int[nums1.length];
 
-        for(int n : nums1) {
-            map.put(n, -1);
+        for (int i = 0; i < nums1.length; i++) {
+            map.put(nums1[i], i);
         }
 
-        for(int i = nums2.length-1; i >= 0; i--) {
+        for (int i = nums2.length - 1; i >= 0; i--) {
             int element = nums2[i];
-            while(!stack.isEmpty()) {
-                if(element >= stack.peek()) {
-                    stack.pop();
-                }
-                else {
-                    break;
-                }
+            while (!stack.isEmpty() && element >= stack.peek()) {
+                stack.pop();
             }
 
-            if(map.get(element) != null) {
-                if(!stack.isEmpty()) {
-                    map.put(element, stack.peek());
-                }
+            if (map.containsKey(element)) {
+                int index = map.get(element);
+                if (!stack.isEmpty())
+                    res[index] = stack.peek();
+                else
+                    res[index] = -1;
             }
 
             stack.push(element);
         }
 
-        return map.values().stream()
-                    .mapToInt(Integer::intValue)
-                    .toArray();
+        // return map.values().stream()
+        // .mapToInt(Integer::intValue)
+        // .toArray();
+        return res;
     
     }
 }
