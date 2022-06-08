d95f2ea4ab5d7cc450b1cf421ddb26047c3b17cf
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-06-08 10:01:41 +0900

    Time: 10 ms (90.32%), Space: 54.8 MB (54.86%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.java
+++ 503-next-greater-element-ii/503-next-greater-element-ii.java
@@ -1,31 +1,18 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-
-        int[] circularArray = new int[nums.length*2];
-        for(int i = 0; i < circularArray.length; i++) {
-            circularArray[i] = nums[i%nums.length];
-        }
-
-        int[] res = new int[circularArray.length];
+        int[] res = new int[nums.length];
         Deque<Integer> stack = new ArrayDeque<>();
 
-        for(int i = circularArray.length-1; i >= 0; i--) {
-            int element = circularArray[i];
+        for(int i = nums.length * 2 - 1; i >= 0; i--) {
+            int element = nums[i % nums.length];
             while(!stack.isEmpty() && element >= stack.peek()) {
                 stack.pop();
             }
 
-            if(!stack.isEmpty()) {
-                res[i] = stack.peek();
-            }
-            else {
-                res[i] = -1;
-            }
-
+            res[i % nums.length] = stack.isEmpty() ? -1 : stack.peek();
             stack.push(element);
         }
 
-        return Arrays.copyOf(res, nums.length);
-    
+        return res;
     }
 }
