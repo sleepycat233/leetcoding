714ccea3adf4e2aac604d529650dfab9a2323484
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-06-23 22:32:45 +0900

    Time: 2 ms (94.69%), Space: 44.2 MB (69.84%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,6 +1,39 @@
 class Solution {
+    //maxheap
     public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        return nums[nums.length - k];
+        int heapSize = nums.length;
+        buildMaxHeap(nums, nums.length);
+        for(int i = nums.length - 1; i > nums.length - k; i--) {
+            swap(nums, 0, heapSize-1);
+            heapSize--;
+            maxHeapify(nums, 0, heapSize);
+        }
+        return nums[0];
+    }
+
+    private void buildMaxHeap(int[] nums, int heapSize) {
+        for(int i = heapSize / 2 - 1; i >= 0; i--) {
+            maxHeapify(nums, i, heapSize);
+        }
+    }
+
+    private void maxHeapify(int[] nums, int p, int heapSize) {
+        int l = p * 2 + 1, r = p * 2 + 2, largest = p;
+        if (l < heapSize && nums[l] > nums[largest]) {
+            largest = l;
+        }
+        if (r < heapSize && nums[r] > nums[largest]) {
+            largest = r;
+        }
+        if(p != largest) {
+            swap(nums, p, largest);
+            maxHeapify(nums, largest, heapSize);
+        }
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
