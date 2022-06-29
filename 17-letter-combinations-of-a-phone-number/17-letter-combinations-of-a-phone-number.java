ac5eacb15f5b2f197ea1bfed4354a9a921b92a2b
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-06-29 21:17:53 +0900

    Time: 1 ms (94.21%), Space: 42.8 MB (46.44%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -4,26 +4,22 @@ class Solution {
             '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz");
 
     public List<String> letterCombinations(String digits) {
-
         List<String> res = new ArrayList<>();
-        List<String> letters = new ArrayList<>();
-                if(digits.isEmpty()) return res;
-        for(int i = 0; i < digits.length(); i++) {
-            letters.add(mapOfLetters.get(digits.charAt(i)));
-        }
-        combine(letters, 0, res, new char[digits.length()], 0);
+        if(digits.isEmpty()) return res;
+
+        combine(digits, res, new char[digits.length()], 0);
         return res;
     }
 
-    private void combine(List<String> letters, int n, List<String> res, char[] run, int size) {
-        if(size == letters.size()) {
+    private void combine(String digits, List<String> res, char[] run, int index) {
+        if(index == digits.length()) {
             res.add(new String(run));
         }
         else {
-            String group = letters.get(n);
+            String group = mapOfLetters.get(digits.charAt(index));
             for(int i = 0; i < group.length(); i++) {
-                run[size] = group.charAt(i);
-                combine(letters, n+1, res, run, size+1);
+                run[index] = group.charAt(i);
+                combine(digits, res, run, index+1);
             }
         }
     }
