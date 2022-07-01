7c9bc5d2e1f73a3af3cfc835803dbcb2460a66ff
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-07-01 16:58:50 +0900

    Time: 2 ms (89.67%), Space: 42.5 MB (12.27%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,46 +1,30 @@
 class Solution {
+    Map<Character, Character> pairs;
+
+    public Solution() {
+        pairs = new HashMap<Character, Character>();
+        this.pairs.put(')', '(');
+        this.pairs.put(']', '[');
+        this.pairs.put('}', '{');
+    }
+
     public boolean isValid(String str) {
         char[] ca = str.toCharArray();
         Deque<Character> stack = new ArrayDeque<>();
 
-        for(int i = 0; i < ca.length; i++) {
+        for (int i = 0; i < ca.length; i++) {
             char c = ca[i];
-            if(isClosingBracket(c)) {
-                if(stack.isEmpty()) return false;
-                char top = stack.pop();
-                if(!isPair(top, c)) return false;
-            }
-            else {
+            if (pairs.containsKey(c)) {
+                if (stack.isEmpty())
+                    return false;
+                char left = stack.pop();
+                if (left != pairs.get(c))
+                    return false;
+            } else {
                 stack.push(c);
             }
         }
 
         return stack.isEmpty();
-    }
-
-    private boolean isClosingBracket(char c) {
-        if(c == ')' || c == ']' || c == '}') return true;
-        else return false;
-    }
-
-    private boolean isPair(char l, char r) {
-        switch (l) {
-            case '(':
-                if (r == ')')
-                    return true;
-                break;
-            case '[':
-                if (r == ']')
-                    return true;
-                break;
-            case '{':
-                if (r == '}')
-                    return true;
-                break;
-
-            default:
-                break;
-        }
-        return false;
     }
 }
