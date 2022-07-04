b9d837122b093d273035948387f367290838c74e
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-07-04 19:07:24 +0900

    Time: 270 ms (40.01%), Space: 16.8 MB (74.92%) - LeetHub

---
+++ 135-candy/135-candy.py
@@ -0,0 +1,16 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        left = [0] * n
+
+        left[0] = 1
+        for i in range(1, n):
+            left[i] = left[i-1]+1 if ratings[i] > ratings[i-1] else 1
+
+        right = 1
+        res = max(right, left[n-1])
+        for j in range(n-2, -1, -1):
+            right = right+1 if ratings[j] > ratings[j+1] else 1
+            res += max(right, left[j])
+
+        return res
