ca3c983b4dfe215e97d328b5a2f87ebd92f33e0e
sleepycat233 <77555918+sleepycat233@users.noreply.github.com>
2022-07-15 16:10:16 +0900

    Time: 0 ms (100.00%), Space: 42.5 MB (85.81%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,56 +1,34 @@
 class Solution {
-public List<String> generateParenthesis(int n) {
-return generateCombinationsTest(new Character[]{'(', ')'}, new int[]{n, n});
-    }
-
-    public <E> List<String> generateCombinationsTest(E[] elements, int[] quantity) {
+char[] elements = {'(', ')'};
+    public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
-        if(elements.length != quantity.length) return res;
-
-        int nOfSlot = 0;
-        for(int q : quantity) nOfSlot += q;
-
-        helper(res, (E[]) new Object[nOfSlot], 0, new int[quantity.length], elements, quantity);
-        res = res.stream()
-                .filter(str -> validate(str))
-                .collect(Collectors.toList());
+        helper(res, new char[n*2], new int[]{0, 0}, 0, 0, n);
 
         return res;
     }
 
-    private <E> void helper(List<String> res, E[] run, int ri, int[] runQty, E[] elements, int[] quantity) {
-        if(ri == run.length) {
-            StringBuilder sb = new StringBuilder();
-            for(E e : run) {
-                sb.append(e);
-            }
-            res.add(sb.toString());
+    private void helper(List<String> res, char[] run, int[] runQty, int ir, int balance, int n) {
+        if(balance < 0) return;
+        else if(ir == run.length) {
+            res.add(new String(run));
         }
         else {
             for(int i = 0; i < elements.length; i++) {
                 runQty[i]++;
-                if (runQty[i] > quantity[i]) {
+                if(runQty[i] > n) {
                     runQty[i]--;
                     continue;
                 }
 
-                run[ri] = elements[i];
-                helper(res, run, ri+1, runQty, elements, quantity);
+                char e = elements[i];
+                run[ir] = e;
+                if(e == '(')
+                    helper(res, run, runQty, ir + 1, balance+1, n);
+                else
+                    helper(res, run, runQty, ir + 1, balance-1, n);
+
                 runQty[i]--;
             }
         }
-    }
-
-    private boolean validate(String str) {
-        int balance = 0;
-
-        for(char c : str.toCharArray()) {
-            if(c == ')') balance--;
-            else balance++;
-
-            if(balance < 0) return false;
-        }
-
-        return balance == 0;
     }
 }
